b239799b420e8a840e65093845ef33a69373ca01
Jonny Reading <jonnyreading@gmail.com>
2020-10-13 23:10:11 +0100

    css

--- src/containers/MusicChart.js
+++ src/containers/MusicChart.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import SongList from "../components/SongList";
+import ChartHeader from "../components/ChartHeader";
 import "./MusicChart.css";
 
 export default class MusicChart extends Component {
@@ -9,16 +10,22 @@ export default class MusicChart extends Component {
         this.state = {
             songs: []
         }
+        this.fetchUrl = this.fetchUrl.bind(this);
     }
 
     componentDidMount() {
-        const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+        this.fetchUrl();
+    }
+
+    fetchUrl(limit = 10) {
+        const url = `https://itunes.apple.com/gb/rss/topsongs/limit=${limit}/json`;
         fetch(url).then(res => res.json()).then(data => this.setState({ songs: data.feed.entry }));
     }
 
     render() {
         return (
             <div id="chart">
+                <ChartHeader length={this.state.songs.length} fetch={this.fetchUrl} />
                 <SongList songs={this.state.songs} />
             </div>
         )
